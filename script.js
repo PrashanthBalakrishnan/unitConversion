6eb759a258c7dedfea140e011763b24afc475302
unknown <prashanthdilshaan@gmail.com>
2022-06-17 21:58:12 -0400

    changed UI
    changed up UI bigger backround and added padding to text.

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ function myFunction() {
     let feetToMeters = document.getElementById("feet--meters");
 
 
-    metersToFeet.innerHTML = (num + " Meters = " + (num * 3.28084).toFixed(3) + " feet " + " | ");
-    feetToMeters.innerText = (num + " feet = " + (num * .3048).toFixed(3) + " meters");
+    metersToFeet.innerText = `${num} Meters =  ${num * (3.28084).toFixed(3)}  feet `;
+    feetToMeters.innerText = `| ${num}  feet = ${num * (.3048).toFixed(3)}  meters`;
 
     let litersToGallon = document.getElementById("liters--gallons");
     let gallonToLiters = document.getElementById("gallons--liters");
 
-    litersToGallon.innerHTML = (num + " liters = " + (num * 0.264172).toFixed(3) + " gallon")
-    gallonToLiters.innerHTML = (num + " gallons = " + (num * 3.78541).toFixed(3) + " liters");
+    litersToGallon.innerText = `${num} + liters =   ${num * (0.264172).toFixed(3)}  gallon`;
+    gallonToLiters.innerText = `| ${num} + gallons =   ${num * (3.78541).toFixed(3)} liters`;
 
 
     let killosToPounds = document.getElementById("kilograms--pounds");
     let poundsToKillos = document.getElementById("pounds--kilograms");
 
-    killosToPounds.innerHTML = (num + " kilograms = " + (num * 2.20462).toFixed(3) + " pounds")
-    poundsToKillos.innerHTML = (num + " pounds = " + (num * 0.453592).toFixed(3) + " liters");
+    killosToPounds.innerText = `${num} kilograms = ${num * (2.20462).toFixed(3)} pounds `;
+    poundsToKillos.innerText = `| ${num} pounds = ${num * (0.453592).toFixed(3)} liters`;
 
 
 
